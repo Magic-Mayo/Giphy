49698fd9d8f7029f4c5aa4942329e17ca13a3111
MagicMayo <mike.mayoii@gmail.com>
2019-06-19 10:44:00 -0700

    made load button functional and moved it into the ajax call so it would load more gifs when you press the button. started a click function for animating the gifs when you click on them. gave the load button a border and css hover function

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -3,6 +3,8 @@ const gif = $('.GIF');
 const buttons = $('.buttons');
 const gifIt = $('.js-GIF-it');
 const add = $('.js-add');
+let gifCount = 0;
+let gifCounter = 4;
 
 for (let i=0; i<fail.length; i++){
     const btn = $('<button>').text(fail[i]).addClass('js-GIF-it');
@@ -13,7 +15,6 @@ add.on('click', function(){
     $('<button>').appendTo('.buttons').text(gif.val()).attr('data-gif', gif.val()).addClass('js-GIF-it')
 })
 
-let gifCount = 0
 $(document).on('click', '.js-GIF-it', function(){
     const text = $(this).attr('data-gif');
     const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=8dZZXNK8KMqOf8ctUav0BuFe8JYedKdP&limit=50&q=" + text + "+fail";
@@ -23,26 +24,43 @@ $(document).on('click', '.js-GIF-it', function(){
     })
     .then(function(response) {
         const result = response.data;
+        let rating;
+        let gifRate;
         for (let i = 0; i<4; i++){
             const img = $('<img>');
-            const div = $('<div>')
-            gifCount++;
+            const div = $('<div>');
             const rating = result[i].rating.toUpperCase();
             const gifRate = $('<p>Rating: ' + rating + '</p>')
-            img.attr('src', result[i].images.fixed_height_still.url).addClass('img-fluid');
+            gifCount++;
+            img.attr('src', result[i].images.fixed_height_still.url);
             console.log(img.attr('src'))
             div.append(gifRate);
             div.prepend(img);
-            $('.result').append(div);
+            $(div).appendTo('.result').addClass('js-gif-click');
+            console.log(gifCount)
         }
-        $('.container').append('<button class="btn load">Load more...</button>')
+        $('.load-btn').html('<button class="btn border-info load">Load more...</button>')
+        $(document).on('click', '.load', function(){
+            gifCounter = gifCounter+4;
+            console.log(gifCounter);
+            console.log(gifCount);
+            
+            for (let i = gifCount; i<gifCounter; i++){
+                const img = $('<img>');
+                const div = $('<div>');
+                const rating = result[i].rating.toUpperCase();
+                const gifRate = $('<p>Rating: ' + rating + '</p>') 
+                img.attr('src', result[i].images.fixed_height_still.url);
+                console.log(img.attr('src'))
+                div.append(gifRate);
+                div.prepend(img);
+                $(div).appendTo('.result').addClass('js-gif-click');
+            }
+            gifCount = gifCount + 4
+                console.log(response)
+        })
+        
+        $(document).on('click', '.js-gif-click', function(){
+        })
     })
-})
-
-// let gifCount;
-let imageURL;
-$(document).on('click', '.load', function(){
-    gifCount += gifCount;
-
-    // for (let i = gifCount; )
 })
