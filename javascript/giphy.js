54e7c0d48441171cb8b7b486896383bcc41d058c
MagicMayo <mike.mayoii@gmail.com>
2019-06-19 16:52:03 -0700

    made max height 200px for all images. working on getting counter to function correctly in js so each time the same button is hit it will return new images. also made function to change source on images to the originial gif url on click. need to get function to work with a second click and changing src back to still image

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -8,59 +8,68 @@ let gifCounter = 4;
 
 for (let i=0; i<fail.length; i++){
     const btn = $('<button>').text(fail[i]).addClass('js-GIF-it');
-    btn.appendTo('.buttons').attr('data-gif', fail[i])
+    btn.appendTo('.buttons').attr('data-gif', fail[i]);
 }
 
 add.on('click', function(){
-    $('<button>').appendTo('.buttons').text(gif.val()).attr('data-gif', gif.val()).addClass('js-GIF-it')
+    $('<button>').appendTo('.buttons').text(gif.val()).attr('data-gif', gif.val()).addClass('js-GIF-it');
 })
 
 $(document).on('click', '.js-GIF-it', function(){
     const text = $(this).attr('data-gif');
     const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=8dZZXNK8KMqOf8ctUav0BuFe8JYedKdP&limit=50&q=" + text + "+fail";
+    // gifCounter = gifCounter+4;
+    console.log(gifCounter);
+
     $.ajax({
         url: queryURL,
         method: "GET"
     })
     .then(function(response) {
         const result = response.data;
-        let rating;
-        let gifRate;
-        for (let i = 0; i<4; i++){
+        for (let i = gifCount; i<gifCounter; i++){
+            console.log(gifCount)
             const img = $('<img>');
             const div = $('<div>');
             const rating = result[i].rating.toUpperCase();
             const gifRate = $('<p>Rating: ' + rating + '</p>')
             gifCount++;
-            img.attr('src', result[i].images.fixed_height_still.url);
-            console.log(img.attr('src'))
+            img.attr('src', result[i].images.fixed_height_still.url).attr('data-name', i).attr('data-click', 'false');
             div.append(gifRate);
             div.prepend(img);
             $(div).appendTo('.result').addClass('js-gif-click');
             console.log(gifCount)
         }
+        
         $('.load-btn').html('<button class="btn border-info load">Load more...</button>')
         $(document).on('click', '.load', function(){
             gifCounter = gifCounter+4;
-            console.log(gifCounter);
-            console.log(gifCount);
             
-            for (let i = gifCount; i<gifCounter; i++){
+            for (let j = gifCount; j<gifCounter; j++){
                 const img = $('<img>');
                 const div = $('<div>');
-                const rating = result[i].rating.toUpperCase();
+                const rating = result[j].rating.toUpperCase();
                 const gifRate = $('<p>Rating: ' + rating + '</p>') 
-                img.attr('src', result[i].images.fixed_height_still.url);
+                img.attr('src', result[j].images.fixed_height_still.url).attr('data-name', j).attr('data-click', 'false');
                 console.log(img.attr('src'))
                 div.append(gifRate);
                 div.prepend(img);
                 $(div).appendTo('.result').addClass('js-gif-click');
             }
             gifCount = gifCount + 4
-                console.log(response)
+            console.log(response)
         })
         
-        $(document).on('click', '.js-gif-click', function(){
+        $(document).on('click', 'img', function(){
+            if ($(this).data('click') === true){
+                console.log()
+                $(this).attr('src', result[$(this).data('name')].images.fixed_height_still.url).attr('data-click', 'false')
+            }
+            
+            else if ($(this).data('click') === false){
+                $(this).attr('src', result[$(this).data('name')].images.original.url).attr('data-click', 'true');
+            }
         })
     })
+    gifCount = gifCount + 4
 })
